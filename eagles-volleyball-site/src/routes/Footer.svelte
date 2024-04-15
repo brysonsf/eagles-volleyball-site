07e6c3772543153ee8bba4aaac5f0bdb845d4135
Bryson Sullivan-Farrell <44358513+brysonsf@users.noreply.github.com>
2024-04-15 23:12:06 +0000

    added footer

---
+++ eagles-volleyball-site/src/routes/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+    <em>Developed by Bryson SF &copy;2024</em> for Eagles Volleyball Halifax
+</footer>
+<style>
+    footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
