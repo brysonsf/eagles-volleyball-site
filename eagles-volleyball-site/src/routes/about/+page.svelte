ede25f0cbd9cfdd3a31d6030d86670720cd319ac
bryson <br441570@dal.ca>
2024-04-16 12:30:23 -0300

    added other coaches

--- eagles-volleyball-site/src/routes/about/+page.svelte
+++ eagles-volleyball-site/src/routes/about/+page.svelte
@@ -17,29 +17,48 @@
 	</p>
 
 	<h1>Eagles Coaching Staff</h1>
-	<div style="width: 100%;">
-		<div style="width: 50%;">
-			<CoachCard>
-				<span slot="name"> Bryson Sullivan-Farrell </span>
-				<span slot="role"> Eaglets Youth Coach</span>
-				<span slot="details">10-yr volleyball athelete both indoor and beach. Experience coaching HS volleyball in Toronto. </span>
-				<span slot="hometown"> Toronto, Ontario, Canada </span>
-				<span slot="email"> brysonsf@gmail.com </span>
-			</CoachCard>
-		</div>
+		<CoachCard>
+			<span slot="name"> Bryson Sullivan-Farrell </span>
+			<span slot="role"> Eaglets Head Coach</span>
+			<span slot="details"> Safe Sport Certified. 10-yr volleyball athelete. Experience coaching HS volleyball. </span>
+			<span slot="hometown"> Toronto, Ontario, Canada </span>
+			<span slot="email"> brysonsf@gmail.com </span>
+			<!--<span slot="picture"><img src={Bryson} alt="Bryson Pic"></span>-->
+		</CoachCard>
+		<CoachCard>
+			<span slot="name"> Jasmine Chiasson </span>
+			<span slot="role"> Technical Director</span>
+			<!--<span slot="details"></span>
+			<span slot="hometown"></span>
+			<span slot="email"></span>-->
+			<span slot="picture"><img src={Jasmine} alt="Jasmine Pic"></span>
+		</CoachCard>
 		<br>
-		
-		<div style="width: 50%;">
-			<CoachCard>
-				<span slot="name"> Jasmine Chiasson </span>
-				<!--<span slot="role"></span>
-				<span slot="details"></span>
-				<span slot="hometown"></span>
-				<span slot="email"></span>-->
-				<span slot="picture"><img src={Jasmine} alt="Jasmine Pic"></span>
-			</CoachCard>
-		</div>
-	</div>
+		<CoachCard>
+			<span slot="name"> Curtis Robarts </span>
+			<span slot="role"> Boys Head Coach</span>
+			<!--<span slot="details"> Safe Sport Certified. 10-yr volleyball athelete. Experience coaching HS volleyball. </span>
+			<span slot="hometown"> Toronto, Ontario, Canada </span>
+			<span slot="email"> brysonsf@gmail.com </span>
+			<span slot="picture"><img src={Curtis} alt="Curtis Pic"></span>-->
+		</CoachCard>
+		<CoachCard>
+			<span slot="name"> Sophie Surette </span>
+			<span slot="role"> Girls Head Coach - Team Apex</span>
+			<!--<span slot="details"></span>
+			<span slot="hometown"></span>
+			<span slot="email"></span>
+			<span slot="picture"><img src={Sophie} alt="Sophie Pic"></span>-->
+		</CoachCard>
+		<br>
+		<CoachCard>
+			<span slot="name"> Sarah van den Heuvel </span>
+			<span slot="role"> Girls Head Coach - Team Visionaries</span>
+			<!--<span slot="details"></span>
+			<span slot="hometown"></span>
+			<span slot="email"></span>
+			<span slot="picture"><img src={Sarah} alt="Sarah Pic"></span>-->
+		</CoachCard>
 	<!-- card display -->
 
 </div>
