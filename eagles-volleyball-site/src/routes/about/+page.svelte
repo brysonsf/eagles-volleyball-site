96853c3d16817f08ae53c1c36c3251457f7026fa
Bryson Sullivan-Farrell <44358513+brysonsf@users.noreply.github.com>
2024-04-18 11:15:53 +0000

    added image control button to coach page

--- eagles-volleyball-site/src/routes/about/+page.svelte
+++ eagles-volleyball-site/src/routes/about/+page.svelte
@@ -1,6 +1,14 @@
 <script>
 	import CoachCard from "../CoachCard.svelte";
 	import Jasmine from "$lib/images/jasmine.png";
+	function changePicture(input){
+		if(document.getElementById(input)?.style["display"]==="none"){
+			document.getElementById(input).style["display"] = "flex";
+		}else{
+			document.getElementById(input).style["display"] = "none";
+		}
+		return null;
+	}
 </script>
 <svelte:head>
 	<title>About</title>
@@ -17,22 +25,29 @@
 	</p>
 
 	<h1>Eagles Coaching Staff</h1>
-		<CoachCard >
-			<span slot="name"> Bryson Sullivan-Farrell </span>
-			<span slot="role"> Eaglets Head Coach</span>
-			<span slot="details"> Safe Sport Certified. 10-yr volleyball athelete. Experience coaching HS volleyball. </span>
-			<span slot="hometown"> Toronto, Ontario, Canada </span>
-			<span slot="email"> brysonsf@gmail.com </span>
-			<!--<span slot="picture"><img src={Bryson} alt="Bryson Pic"></span>-->
-		</CoachCard>
-		<CoachCard>
-			<span slot="name"> Jasmine Chiasson </span>
-			<span slot="role"> Technical Director</span>
-			<!--<span slot="details"></span>
-			<span slot="hometown"></span>
-			<span slot="email"></span>-->
-			<span slot="picture"><img src={Jasmine} alt="Jasmine Pic"></span>
-		</CoachCard>
+		<div style="width:100%;">
+			<div style="width:50%;">
+				<CoachCard>
+					<span slot="name"> Bryson Sullivan-Farrell </span>
+					<span slot="role"> Eaglets Head Coach</span>
+					<span slot="details"> Safe Sport Certified. 10-yr volleyball athelete. Experience coaching HS volleyball. </span>
+					<span slot="hometown"> Toronto, Ontario, Canada </span>
+					<span slot="email"> brysonsf@gmail.com </span>
+					<!--<span slot="picture"><img src={Bryson} alt="Bryson Pic"></span>-->
+				</CoachCard>
+			</div>
+			<div style="width: 50%;">
+				<button on:click={() => changePicture(Jasmine)}>Show/Hide Image</button>
+				<CoachCard>
+					<span slot="name"> Jasmine Chiasson </span>
+					<!--<span slot="role"></span>
+					<span slot="details"></span>
+					<span slot="hometown"></span>
+					<span slot="email"></span>-->
+					<span slot="picture"><img id={Jasmine} src={Jasmine} alt="Jasmine Pic" style="display:none;"></span>
+				</CoachCard>
+			</div>
+		</div>
 		<br>
 		<CoachCard>
 			<span slot="name"> Curtis Robarts </span>
@@ -59,6 +74,4 @@
 			<span slot="email"></span>
 			<span slot="picture"><img src={Sarah} alt="Sarah Pic"></span>-->
 		</CoachCard>
-	<!-- card display -->
-
-</div>
+		</div>
