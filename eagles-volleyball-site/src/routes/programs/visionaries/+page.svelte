e2cc05d5c7c3c7345dfe9933260dcb221c10fb29
Bryson Sullivan-Farrell <44358513+brysonsf@users.noreply.github.com>
2024-04-22 18:39:02 +0000

    Tiny case fix

--- eagles-volleyball-site/src/routes/programs/visionaries/+page.svelte
+++ eagles-volleyball-site/src/routes/programs/visionaries/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 <svelte:head>
-	<title>team Visionaries - Halifax Bedford Blazing Eagles Youth Girls Competition Team</title>
+	<title>Team Visionaries - Halifax Bedford Blazing Eagles Youth Girls Competition Team</title>
 	<meta name="description" content="Eagles Volleyball Youth Girls Competition Team. Halifax Competitive youth girls volleyball training program.">
 	<meta name="keywords" content="Eagles, Volleyball, Halifax, Volley, Ball, Adult, Co-ed, Drop-In, Competitive, Children, Youth, Teen, Camp, Training, Program, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18">
 	<meta name="author" content="Bryson Sullivan-Farrell">
